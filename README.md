382d59eb67cde71621cc7871bc391134a08ef9d7
zara sultan <zarasultan@zaras-MacBook-Pro.local>
2022-03-03 16:24:01 -0500

    my final game

---
+++ README.md
@@ -0,0 +1,40 @@
+<h1>War</h1>
+ 
+<p>A simple card game typically played by two players using a standard playing card deck.</p>
+
+<p>Technologies used: JavaScript, jQuery, Css and HTML.</p>
+
+<p>Game can be played at <span><a></a>  </span></p>
+
+
+<h2>Game Walkthrough</h2>
+
+<h3>Setup:</h3>
+
+<p>The deck is divided equally among the players, so that each player has a down stack.</p>
+
+<h3>In play:</h3>
+
+<p>After that, the top card of each player's deck is flipped over and compared. The player with the highest card  value wins both cards.</p>
+
+<h3>War:</h3>
+
+<p>If the two cards played are of equal value, then there is a "war". Both players place the next three cards face down and then another card face-up and is compared as normal. The player with the highest card then wins all of the "war" cards.</p>
+
+
+<h3>Game over:</h3>
+
+<p>The game ends when one player holds all of the cards.</p>
+
+
+<h4>Future enhancements:</h4>
+
+<p>Adding a choice to reshuffle the deck when the player thinks all good cards are gone. Also, adding the reinforcement choice where the players draw 2 cards and choose which card will be played.</p>
+
+<h4>Screenshot(s):</h4>
+<p>resources/war screenshots1.png</p></break>
+<p>resources/war screenshots2.png</p></break>
+<p>resources/war screenshots3.png</p></break>
+
+<h5>Notes:</h5>
+<p>CSS animations come from 'jQuery Flip', a fantastic animation library is being maintained by Nattawat Nonsung (author), Jemar Jones and Stijn de Witt. <a href="https://nnattawat.github.io/flip/">JQuery Flip</a>.</p>
---
+++ main.js
@@ -0,0 +1,154 @@
+const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const suits =  ["s", "d", "c", "h"];
+let playerDeck, computerDeck;
+let playerCard, computerCard;
+let gameOver;
+const deckEl = document.querySelector(".deck");
+const playerSlot = document.querySelector(".player-slot");
+const computerSlot = document.querySelector(".computer-slot");
+const msg = document.querySelector(".msg");
+const computerEl = document.querySelector(".computer");
+const playerEl = document.querySelector(".player");
+const slot = document.querySelector(".slot");
+
+function getDeck(){
+  let deck = new Array();
+    for(let i = 0; i < suits.length; i++)
+    {
+        for(let x = 0; x < values.length; x++)
+        {
+            if(typeof values[x] == "number"){
+                 let card = {value: values[x], card: suits[i] + values[x]};
+                 deck.push(card);
+            }else {
+                if(values[x]==="J"){
+                 card = {value: 11, card: suits[i] + values[x] };
+                 deck.push(card);
+                }else if(values[x] === "Q"){ 
+                 card = {value: 12, card: suits[i] + values[x] };
+                 deck.push(card);
+                }else if(values[x]=== "K"){
+                 card = {value: 13, card: suits[i] + values[x] };
+                 deck.push(card);
+                }else if(values[x]=== "A"){
+                 card = {value: 14, card: suits[i] + values[x] };
+                 deck.push(card);
+                }
+            }
+        }
+    }
+ return deck;
+}
+
+const newDeck = getDeck();
+
+function shuffle() {
+    for (var i = newDeck.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = newDeck[i];
+        newDeck[i] = newDeck[j];
+        newDeck[j] = temp;
+    }                   
+}
+	
+function init(){
+	$("#cards-flip").flip({
+	 trigger: 'manual'});
+	msg.style.color = "black";
+	msg.style.fontSize= "30px";
+    shuffle();
+    playerDeck = newDeck.slice(0, 26);
+    computerDeck = newDeck.slice(26, newDeck.length);
+    gameOver = false;
+    msg.innerHTML = "";
+    playerSlot.innerHTML = "";
+    computerSlot.innerHTML = "";
+    computerEl.innerText = computerDeck.length;
+    playerEl.innerText = playerDeck.length;
+    playerSlot.classList.remove("card") ;
+    computerSlot.classList.remove("card");
+
+}
+
+function flip(){
+
+    playerCard= playerDeck.shift();
+    playerSlot.classList.remove(...playerSlot.classList);
+    playerSlot.classList.add("player-slot", "slot", "card","back", playerCard.card);
+    computerCard = computerDeck.shift();
+    computerSlot.classList.remove(...computerSlot.classList)
+    computerSlot.classList.add("computer-slot", "slot", "card","back", computerCard.card);
+    computerEl.innerText = computerDeck.length;
+    playerEl.innerText = playerDeck.length;
+	setTimeout(() =>{
+		$("#cards-flip").flip(true);
+	}, 500);
+	setTimeout(() =>{
+		document.addEventListener("click", handleClick)
+		isRoundWinner()
+	}, 900)
+}
+
+function isRoundWinner(){
+	
+  if(playerCard.value > computerCard.value){
+    msg.innerHTML = "Player Won this round";
+    playerDeck.push(playerCard);
+    playerDeck.push(computerCard);
+
+  }else if(playerCard.value < computerCard.value){
+    msg.innerHTML = "Computer Won this round";
+    computerDeck.push(computerCard);
+    computerDeck.push(playerCard);
+
+  }else if (playerCard.value === computerCard.value){
+    msg.innerHTML = "WAR, flip the cards again";
+    playerDeck.splice(playerCard, 3);
+     computerDeck.splice(computerCard, 3);
+       if(playerCard.value > computerCard.value){
+            msg.innerHTML = "Player Won this round";
+            playerDeck.push(playerCard);
+            playerDeck.push(computerCard);
+			computerEl.innerText = computerDeck.length;
+            playerEl.innerText = playerDeck.length;
+			
+        }else if(playerCard.value < computerCard.value){
+            msg.innerHTML = "Computer Won this round";
+            computerDeck.push(computerCard);
+            computerDeck.push(playerCard);
+			computerEl.innerText = computerDeck.length ;
+            playerEl.innerText = playerDeck.length;
+          }
+  	}
+    
+  if(playerDeck.length === 0){
+	msg.style.color= "red";
+	msg.style.fontSize= "50px";
+    msg.innerHTML = "Computer Won!";
+    gameOver = true;
+  }else if(computerDeck.length === 0){
+	msg.style.color= "blue";
+    msg.innerHTML = "Player Won! 🏆";
+	msg.style.fontSize= "50px";
+    gameOver = true;
+  }
+
+}
+
+function handleClick(){
+	document.removeEventListener("click", handleClick);
+	$("#cards-flip").flip(false);
+	if(gameOver){
+		document.addEventListener("click", handleClick)	
+		setTimeout(() => {
+		   init();
+		   return;
+		}, 3000);
+	}else{
+	  flip();
+	 }
+}
+
+document.addEventListener("click", handleClick);
+	
+init();
